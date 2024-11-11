2855556ea739e44d3ea81e7de9d64e208118106c
Duncan R <duncansrendall@gmail.com>
2024-11-11 19:15:57 +0000

    added compute shader file to compile for inline ray-tracing

---
+++ src/ascend/Shader/ComputeRaytracing.hlsl
@@ -0,0 +1,21 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+#ifndef COMPUTERAYTRACING_HLSL
+#define COMPUTERAYTRACING_HLSL
+
+[numthreads(1, 1, 1)]
+void CSMain(uint3 DTid : SV_DispatchThreadID)
+{
+
+}
+
+#endif // COMPUTERAYTRACING_HLSL
