c8c24a16b1541e91bf1f042b14e67519a1cb85e3
Duncan R <duncansrendall@gmail.com>
2025-01-24 12:57:35 +0000

    Added Buffer types adapted from D3D12 MiniEngine Sample

--- src/ascend/DX12/GraphicsTypes.h
+++ src/ascend/DX12/GraphicsTypes.h
@@ -1,6 +1,7 @@
 #include "DX12.h"
 
-// to fill with graphics types 
+//                                          Fence
+//----------------------------------------------------------------------------------------------
 struct Fence
 {
 	ComPtr<ID3D12Fence> DX12fence;
@@ -12,3 +13,82 @@ struct Fence
 	bool Signaled(uint64_t fenceValue);
 	void Clear(uint64_t fenceValue);
 };
+
+//                                         Buffer
+//----------------------------------------------------------------------------------------------
+class Buffer
+{
+public:
+	Buffer();
+	~Buffer();
+
+
+	void* Map();
+	void Unmap(size_t begin = 0, size_t end = -1);
+	size_t GetBufferSize() const { return m_bufferSize; }
+	D3D12_GPU_VIRTUAL_ADDRESS GetGpuVirtualAddress() const { return m_gpuVirtualAddress; }
+
+protected:
+
+	ComPtr<ID3D12Resource> m_resource;
+	D3D12_RESOURCE_STATES m_usageState;
+	D3D12_RESOURCE_STATES m_transitioningState;
+
+	D3D12_GPU_VIRTUAL_ADDRESS m_gpuVirtualAddress;
+	size_t m_bufferSize;
+};
+
+//                                      Upload Buffer
+//----------------------------------------------------------------------------------------------
+class UploadBuffer : public Buffer
+{
+public:
+	UploadBuffer();
+
+	void Create(const std::wstring& name, size_t BufferSize);
+
+};
+
+//                                      GPU Buffer
+//----------------------------------------------------------------------------------------------
+class GpuBuffer : public Buffer
+{
+public:
+	GpuBuffer();
+
+	void Create(const std::wstring& name, uint32_t NumElements, uint32_t ElementSize, const UploadBuffer& srcData, uint32_t srcOffset);
+	const D3D12_CPU_DESCRIPTOR_HANDLE& GetUAV() const { return m_uav; }
+	const D3D12_CPU_DESCRIPTOR_HANDLE& GetSRV() const { return m_srv; }
+
+	D3D12_VERTEX_BUFFER_VIEW VertexBufferView(size_t Offset, uint32_t Size, uint32_t Stride) const;
+	D3D12_INDEX_BUFFER_VIEW IndexBufferView(size_t Offset, uint32_t Size, bool b32Bit = false) const;
+
+
+
+private:
+	D3D12_CPU_DESCRIPTOR_HANDLE m_uav;
+	D3D12_CPU_DESCRIPTOR_HANDLE m_srv;
+
+	uint32_t m_elementCount;
+	uint32_t m_elementSize;
+	D3D12_RESOURCE_FLAGS m_resourceFlags;
+};
+
+
+inline D3D12_VERTEX_BUFFER_VIEW GpuBuffer::VertexBufferView(size_t Offset, uint32_t Size, uint32_t Stride) const
+{
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
+	vertexBufferView.BufferLocation = m_gpuVirtualAddress + Offset;
+	vertexBufferView.SizeInBytes = Size;
+	vertexBufferView.StrideInBytes = Stride;
+	return vertexBufferView;
+}
+
+inline D3D12_INDEX_BUFFER_VIEW GpuBuffer::IndexBufferView(size_t Offset, uint32_t Size, bool b32Bit) const
+{
+	D3D12_INDEX_BUFFER_VIEW indexBufferView;
+	indexBufferView.BufferLocation = m_gpuVirtualAddress + Offset;
+	indexBufferView.Format = b32Bit ? DXGI_FORMAT_R32_UINT : DXGI_FORMAT_R16_UINT;
+	indexBufferView.SizeInBytes = Size;
+	return indexBufferView;
+}
