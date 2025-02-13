fa563007453220a32bc478ac3476c2b51590944e
Duncan R <duncansrendall@gmail.com>
2025-02-13 09:36:35 +0000

    Change descriptor range to be volatile to supress warning for indexing

--- src/ascend/WorkGraphsDXR.cpp
+++ src/ascend/WorkGraphsDXR.cpp
@@ -569,7 +569,7 @@ void WorkGraphsDXR::LoadRasterAssets()
 	 // Create the root signature.
 	{
 		CD3DX12_DESCRIPTOR_RANGE1 ranges[1];
-		ranges[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DATA_STATIC);
+		ranges[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DATA_VOLATILE);
 
 		CD3DX12_ROOT_PARAMETER1 rootParameters[2];
 		rootParameters[0].InitAsDescriptorTable(1, &ranges[0], D3D12_SHADER_VISIBILITY_PIXEL);
